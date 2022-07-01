e7acbc11592e7b04262f45e82227c66b0d531c29
Ashwini Rathod <rashwini510@gmail.com>
2022-07-01 22:22:03 +0530

    Added: User registration validation tested using junit

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,21 +1,88 @@
 package com.bridgelabz;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class UserRegistrationTest
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+public class UserRegistrationTest {
+    public static UserRegistration userRegistration = new UserRegistration();
 
-    {
-        assertTrue( true );
+    // positive test case  for firstName return true
+    @Test
+    public void validateFirstNameTestReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateFirstName("Ashwini");
+        Assert.assertTrue(actualOutput);
+    }
+
+    // negative test case for firstName return false
+    @Test
+    public void validateFirstNameTestReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateFirstName("ashu");
+        Assert.assertFalse(actualOutput);
+    }
+
+    // positive test case for lastName return true
+    @Test
+    public void validateLastNameTestReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateLastname("Rathod");
+        Assert.assertTrue(actualOutput);
+    }
+
+    // negative test case for lastName return false
+    @Test
+    public void validateLastNameTestReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateLastname("rathod");
+        Assert.assertFalse(actualOutput);
+    }
+
+    // positive test case for email return true
+    @Test
+    public void validateEmailTestReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateEmail("rashwini510@gamil.com");
+        Assert.assertTrue(actualOutput);
+    }
+
+    // negative test case for email return false
+    @Test
+    public void validateEmailTestReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateEmail("abc@com.in.");
+        Assert.assertFalse(actualOutput);
+    }
+
+    // positive test case for mobile number return true
+    @Test
+    public void validateMobileNumberReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateMobileNumber("+91 9689410960");
+        Assert.assertTrue(actualOutput);
+    }
+
+    // negative test case for mobile number return false
+    @Test
+    public void validateMobileNumberReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validateMobileNumber("+919689410960");
+        Assert.assertFalse(actualOutput);
+    }
+
+    // positive test case for password return true
+    @Test
+    public void validatePasswordReturnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validatePassword("AshwiniRathod@16");
+        Assert.assertTrue(actualOutput);
+    }
+
+    // negative test case for password return false
+    @Test
+    public void validatePasswordReturnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean actualOutput = userRegistration.validatePassword("ashwini@16");
+        Assert.assertFalse(actualOutput);
     }
 }
