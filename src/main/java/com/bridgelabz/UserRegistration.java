979a3a970d65a36fe4d6d59916c8859abdaca20e
Ashwini Rathod <rashwini510@gmail.com>
2022-07-01 22:12:42 +0530

    Added: Welcome to user registration junit program

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserRegistration
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Welcome to user registration junit program" );
+    }
+}
